c12922fd6a3c4e30e737d581a5e2b3e45ecd8bf5
tuanvinhtl <tuanvinhtl@gmail.com>
2022-07-03 19:28:32 +0700

    1 file modified

--- src/app/overlay-slot.directive.ts
+++ src/app/overlay-slot.directive.ts
@@ -14,21 +14,22 @@ import moment from 'moment';
 
 @Directive({ selector: '[appRender]' })
 export class OverlaySlotDirective implements AfterViewInit {
+  offsetWidthTimeSlots = 72;
   @Input()
   set range(value) {
     if (!value.totalTime) return;
     const totalTime = value.totalTime;
     const widthTotalPos = this.elementRef.nativeElement.offsetWidth;
-    const scale = 72 / (24 * 3600);
+    const scale = this.offsetWidthTimeSlots / (24 * 3600);
 
     value.booking.order.forEach((order) => {
       // take a look caculate the translation x value
-      // caculate the with of the book related to time range
+      // caculate the width of the box related to time range
       const timeDuring =
         moment(order.end_tine).unix() - moment(order.start_time).unix();
       const posStart =
         moment(order.start_time).unix() - moment(value.start).unix();
-      const ran = posStart * scale + 72;
+      const ran = posStart * scale + this.offsetWidthTimeSlots;
       const ranWidth = timeDuring * scale;
       const child = this.document.createElement('div');
       child.className = 'overlay-slot';
@@ -36,6 +37,7 @@ export class OverlaySlotDirective implements AfterViewInit {
       const cssText = `width: ${ranWidth}px; transform: translateX(${ran}px);`;
       child.style.cssText = cssText;
       this.renderer.appendChild(this.elementRef.nativeElement, child);
+      console.log(scale)
     });
   }
   constructor(
@@ -45,5 +47,7 @@ export class OverlaySlotDirective implements AfterViewInit {
   ) {}
   ngAfterViewInit(): void {
     // console.log(this.elementRef.nativeElement.offsetWidth);
+   
+    // 
   }
 }
